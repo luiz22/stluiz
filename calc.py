24c941a26b1c3e68c9e2b8a686d6d8e3a7ea0ed4
NIS <lewisfiles2005@yahoo.com>
2017-05-27 06:39:56 -0700

    we are here

---
+++ calc.py
@@ -0,0 +1,26 @@
+Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+>>> a=7
+>>> b=9
+>>> a+b =c
+SyntaxError: can't assign to operator
+>>> c =a+b
+>>> c
+16
+>>> a=3,b=4
+SyntaxError: can't assign to literal
+>>> a=5
+>>> c=8
+>>> d=c-a
+>>> d
+3
+>>> int(3)
+3
+>>> int(2.0)
+2
+>>> float(3/2)
+1.0
+>>> print ("this just the beginning")
+this just the beginning
+>>> 
