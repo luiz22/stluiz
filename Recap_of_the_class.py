6f7dbacbf5f58a73a724de71dd92f8413e923302
NIS <lewisfiles2005@yahoo.com>
2017-05-31 13:23:44 -0700

    heya

---
+++ Recap_of_the_class.py
@@ -0,0 +1,43 @@
+#Recap of the class
+
+# my_age_range = 2,6,8,9  #Numbers
+# print(my_age_range)
+
+# my_name = "Louis"   #String
+# print my_name[4]
+# #print(my_name)
+
+# x = {"first": "ST Louis", "Last": "Abeku"}  #Dictionary
+# print("Abeku")
+# #print(x) 
+# y = {"first": "ST PAul", "Last": "Kwame"}
+# print("ST PAul")
+# #users = [x,y]
+# #print(users)
+
+# x = [1,2,3]    #List
+# print(x)
+
+# x= (1,2,3,)    #Turble
+# print(x)
+
+
+
+
+
+
+char = raw_input("what is your fullname. ")
+#new = "Louis"
+#print(char)
+if char == "kwame":
+ 	print"hey thats cool"
+elif char == "Ama":
+	print("not now")
+else:
+ 	print("try again guy!. ")
+
+
+    
+
+
+
