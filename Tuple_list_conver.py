fd9c9e663a88fdf947a13418c535c6ab9c6ac9e4
NIS <lewisfiles2005@yahoo.com>
2017-06-01 07:12:20 -0700

    conversion

---
+++ Tuple_list_conver.py
@@ -0,0 +1,15 @@
+#Example of Tuple and list conversion
+#surrounded like a ball
+
+pi_tuple = (1,3,5,6,8,9) #tuple
+
+new_tuple = list(pi_tuple) #converting a tuple into List
+print(new_tuple)
+
+
+
+new_list = [1,4,8,13] #list
+
+new_convert_list = tuple(new_list) #convert a list inot tuple
+print(new_convert_list)
+
