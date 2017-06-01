0e9f43b0967721de07608ac1bf18c18bbd99b454
NIS <lewisfiles2005@yahoo.com>
2017-06-01 08:08:10 -0700

    performing action number of times

---
+++ loop_script.py
@@ -0,0 +1,20 @@
+#Loop :performing an action for a number times
+
+for x in range(0,10):
+	print(x)
+
+print('\n')
+
+grocery_list = ["Coke","Beer","Orange Juice","Fan yogo"]
+
+for k in grocery_list:
+	print(k)
+
+for x in [2,4,6,8,10]:
+	print(x)
+
+num_list = [[1,2,3],[10,20,30],[100,200,330]]
+
+for x in range(0,3):
+	for y in range(0,3):
+		print(num_list[x][y])
