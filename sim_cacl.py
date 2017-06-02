4a098a29d17d1e9c5e392e1761119c13c5262ede
NIS <lewisfiles2005@yahoo.com>
2017-06-02 12:35:26 -0700

    calc

---
+++ sim_cacl.py
@@ -0,0 +1,29 @@
+
+
+
+# print"Simple calculator"
+# numb1 =int(raw_input("Enter First value"))
+# numb2 =int(raw_input("Enter First value"))
+
+# print' the sum is ........',"numb1 + numb2"
+# print' the sum is ........',"numb1 - numb2"
+# print' the sum is ........',"numb1 / numb2"
+# print' the sum is ........',"numb1 * numb2"
+# print' the sum is ........',"numb1 + numb2"
+
+
+
+print("SIMPLE CALCULATOR! ")
+print('Please enter an integer value:')
+x = input()
+print('Please enter another integer value:')
+y = input()
+num1 = int(x)
+num2 = int(y)
+print(num1, '+', num2, '=', num1 + num2)
+print(num1, '*', num2, '=', num1 * num2)
+print(num1, '/', num2, '=', num1 / num2)
+print(num1, '-', num2, '=', num1 - num2)
+
+
+
